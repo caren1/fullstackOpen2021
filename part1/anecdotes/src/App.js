8cb24ea4b358923e34d728d8f6e84e1f6b615be4
caren1 <czarnocki.w@wp.pl>
2021-08-13 23:03:54 +0200

    1.13* anecdotes, step2

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
 const App = () => {
+
   const anecdotes = [
     'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
@@ -12,17 +13,26 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients',
   ]
-   
+
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0))
   const [selected, setSelected] = useState(0)
 
   const selectNote = (anecdotes) => {
      setSelected(Math.floor(Math.random() * (anecdotes.length - 0 )) + 0);
   }
 
+  const voteForAnecdote = (selected) => {
+    const newPoints = [...points];
+    newPoints[selected]++
+    setPoints(newPoints);
+  }
+
   return (
     <div style={{ textAlign: 'center', padding: '2em'}}>
-      <h1 style={{fontSize: '24px'}}>"{anecdotes[selected]}"</h1>
+      <h1 style={{fontSize: '24px'}}>"{anecdotes[selected]}"</h1><span>currently with - {points[selected]} votes</span>
+      <br />
       <button style={{padding: '1.1em 1em', fontSize:'18px', margin: '1em'}} onClick={() => selectNote(anecdotes)}>next anecdote</button>
+      <button style={{padding: '1.1em 1em', fontSize:'18px', margin: '1em'}} onClick={() => voteForAnecdote(selected)}>like 👍</button>
     </div>
   )
 }
