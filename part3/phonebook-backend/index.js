dfc05cec2bfb1f523b07c52b6999fe7a55752887
caren1 <czarnocki.w@wp.pl>
2021-08-23 21:57:54 +0200

    3.9 phonebook backend, step9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
 app.use(express.json());
 // app.use(morgan('tiny'));
@@ -21,6 +22,7 @@ app.use(express.json());
 // 2nd method
 morgan.token('requestContent', (req, res) =>  JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :requestContent'))
+app.use(cors());
 
 let persons = [
     { 
@@ -112,7 +114,7 @@ app.post('/api/persons', (request, response) => {
         response.json(person);   
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
 });
