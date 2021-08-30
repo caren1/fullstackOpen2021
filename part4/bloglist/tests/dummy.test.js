00feb3c08c3551780c248bab6e548233c6937905
caren1 <czarnocki.w@wp.pl>
2021-08-30 17:36:23 +0200

    4.3 helper functions and unit tests, step1

---
+++ part4/bloglist/tests/dummy.test.js
@@ -0,0 +1,9 @@
+const { expect } = require('@jest/globals');
+const listhelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+    const blogs = [];
+
+    const result = listhelper.dummy(blogs);
+    expect(result).toBe(1);
+})
